499352254a378a5dacbe3625a4dd4d4b4466b641
player31-kks <keumks789@naver.com>
2024-01-16 19:52:05 +0900

    fix: script command and default port

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 export const env = zennv({
   dotenv: true,
   schema: z.object({
-    PORT: z.number().default(8000),
+    PORT: z.number().default(3000),
     HOST: z.string().default('0.0.0.0'),
     DATABASE_CONNECTION: z.string(),
     GOOGLE_CLIENT_ID: z.string().default(''),
