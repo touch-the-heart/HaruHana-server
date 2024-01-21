89478dfac0ce425dae19abb619e33ac3125c5cf1
player31-kks <keumks789@naver.com>
2024-01-21 23:06:04 +0900

    feat: add premission in couple

--- src/module/couple/couple.route.ts
+++ src/module/couple/couple.route.ts
@@ -1,10 +1,19 @@
 import { FastifyInstance } from 'fastify';
 import { getCoupleImageURL, getCouplePageHandler } from './couple.controller';
 import { CoupleImage, CoupleImageType } from './couple.schema';
+import { PERMISSIONS } from '../../config/permissions';
 
 export const coupleRoutes = async (app: FastifyInstance) => {
   const tags = ['couple'];
-  app.get('/page/:date', { schema: { tags } }, getCouplePageHandler);
+  app.get(
+    '/page/:date',
+    { schema: { tags }, preHandler: app.guard.scope(PERMISSIONS['user:*']) },
+    getCouplePageHandler,
+  );
   // app.get('/calendar/:date', getCoupleCalendarHandler);
-  app.post<{ Body: CoupleImageType }>('/image/url', { schema: { body: CoupleImage, tags } }, getCoupleImageURL);
+  app.post<{ Body: CoupleImageType }>(
+    '/image/url',
+    { schema: { body: CoupleImage, tags }, preHandler: app.guard.scope(PERMISSIONS['user:*']) },
+    getCoupleImageURL,
+  );
 };
